d314fde74fa5bc0142b0cd9b1f051b71c1c4af60
Emil Christiansen <emilsen68@gmail.com>
2022-04-22 10:14:02 +0200

    Changing to HID-Project

--- ArduinoDeckConfigs/StandardDeck/StandardDeck.ino
+++ ArduinoDeckConfigs/StandardDeck/StandardDeck.ino
@@ -1,52 +1,10 @@
-#define KEY_RIGHT_SHIFT 0x85
-#define KEY_RIGHT_ALT   0x86
-#define KEY_RIGHT_GUI   0x87
-#define KEY_LEFT_GUI    0x83
-#define KEY_UP_ARROW    0xDA
-#define KEY_DOWN_ARROW  0xD9
-#define KEY_LEFT_ARROW  0xD8
-#define KEY_RIGHT_ARROW 0xD7
-#define KEY_BACKSPACE   0xB2
-#define KEY_TAB         0xB3
-#define KEY_RETURN      0xB0
-#define KEY_ESC         0xB1
-#define KEY_INSERT      0xD1
-#define KEY_DELETE      0xD4
-#define KEY_PAGE_UP     0xD3
-#define KEY_PAGE_DOWN   0xD6
-#define KEY_HOME        0xD2
-#define KEY_END         0xD5
-#define KEY_CAPS_LOCK   0xC1
-#define KEY_F1          0xC2
-#define KEY_F2          0xC3
-#define KEY_F3          0xC4
-#define KEY_F4          0xC5
-#define KEY_F5          0xC6
-#define KEY_F6          0xC7
-#define KEY_F7          0xC8
-#define KEY_F8          0xC9
-#define KEY_F9          0xCA
-#define KEY_F10         0xCB
-#define KEY_F11         0xCC
-#define KEY_F12         0xCD
-#define KEY_F13         0x8D
-#define KEY_F14         0x8C
-#define KEY_F15         0x8D
-#define KEY_F16         0x8E
-#define KEY_F17         0x8F
-#define KEY_F18         0x9C
-#define KEY_F19         0x9D
-#define KEY_F20         0x9E
-#define KEY_F21         0x9F
-#define KEY_F22         0xD8
-#define KEY_F23         0xD9
-#define KEY_F24         0xDA
-#define KEY_F25         0xE8
-#define KEY_LEFT_CTRL   0x98
-#define KEY_NUM_period  0xEB
-
+#define HID_CUSTOM_LAYOUT
+#define LAYOUT_NORWEGIAN
+#include <HID-Project.h>
 #include <Keypad.h>
-#include <Keyboard.h>
+
+#define KEY_F25 0xE8
+
 #define ROWS 3
 #define COLS 5
 
@@ -62,15 +20,15 @@ byte colPins[COLS] = {4,5,6,7,8};
 Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 void b1() {
-  Keyboard.write(KEY_F13);
+  Keyboard.write(MEDIA_PLAY_PAUSE);
 }
 
 void b2() {
-  Keyboard.write(KEY_F14);
+  Keyboard.write(KEY_VOLUME_UP);
 }
 
 void b3() {
-  Keyboard.write(KEY_F15);
+  Keyboard.write();
 }
 
 void b4() {
@@ -86,7 +44,7 @@ void b6() {
 }
 
 void b7() {
-  Keyboard.write(KEY_F19);
+  Keyboard.write(KEY_VOLUME_DOWN);
 }
 
 void b8() {
@@ -122,11 +80,12 @@ void b15() {
 }
 
 void setup() {
-  Serial.begin(9600);
+  // put your setup code here, to run once:
   Keyboard.begin();
-};
-  
+}
+
 void loop() {
+  // put your main code here, to run repeatedly:
   int pressedKey = customKeypad.waitForKey();
   switch (pressedKey){
     case 1:
